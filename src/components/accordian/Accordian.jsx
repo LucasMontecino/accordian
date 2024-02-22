3542888e4ca74f349d5f7fd8cb27a5a6f5e8cc2d
Lucas Montecino <lucasmontecino95@gmail.com>
2024-02-22 16:08:38 -0300

    finish-accordian

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -4,29 +4,63 @@ import "./styles.css";
 
 function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enabledMultiselect, setEnabledMultiselect] = useState(false);
+  const [multiSelected, setMultiSelected] = useState([]);
+
+  function handleMultiSelect(id) {
+    let copyMulti = [...multiSelected];
+
+    const findIndex = copyMulti.indexOf(id);
+
+    if (findIndex === -1) {
+      copyMulti.push(id);
+    } else {
+      copyMulti.splice(findIndex, 1);
+    }
+    setMultiSelected(copyMulti);
+  }
 
   function handleSimpleSelect(id) {
     setSelected(id);
     if (selected === id) setSelected(null);
   }
 
-  console.log(selected);
+  function handleButton() {
+    setEnabledMultiselect(!enabledMultiselect);
+    setSelected(null);
+    setMultiSelected([]);
+  }
+
+  console.log(selected, enabledMultiselect, multiSelected);
 
   return (
     <div className="wrapper">
-      <button className="btn">Enabled multiselection</button>
+      <button
+        className={`${enabledMultiselect ? "btn err" : "btn"}`}
+        id="my-btn"
+        onClick={handleButton}
+      >
+        {enabledMultiselect
+          ? "Disabled multiselection"
+          : "Enabled multiselection"}
+      </button>
       {data && data.length ? (
         data.map((item) => (
           <div
-            onClick={() => handleSimpleSelect(item.id)}
+            onClick={() =>
+              enabledMultiselect
+                ? handleMultiSelect(item.id)
+                : handleSimpleSelect(item.id)
+            }
             key={item.id}
             className="container"
           >
             <h2 className="title">{item.question}</h2>
             <span className="plus">+</span>
-            {selected !== null && selected === item.id && (
+            {(multiSelected.indexOf(item.id) !== -1 && (
               <p className="answer">{item.answer}</p>
-            )}
+            )) ||
+              (selected === item.id && <p className="answer">{item.answer}</p>)}
           </div>
         ))
       ) : (
